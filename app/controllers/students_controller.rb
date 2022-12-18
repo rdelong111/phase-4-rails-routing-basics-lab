56bb4e206e75366ea488f951998c6ce936fcc259
Ryan DeLong <rdelong111@gmail.com>
2022-12-18 07:51:48 -0600

    commit tree 153216875547e406fb945e9bec8874203e0c8d0d

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+  def index
+    render json: Student.all
+  end
+
+  def grades
+    render json: Student.order(grade: :desc)
+  end
+
+  def highest_grade
+    render json: Student.order(:grade).last
+  end
+end
